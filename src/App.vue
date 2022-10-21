e67e6cd7223a1c6f4a49def9ba5a6b012e808906
Marco Antonio <caldeiran391@gmail.com>
2022-10-21 17:46:13 -0300

    Meu primeiro commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar />
+  <router-view />
+  <Footer />
+</template>
+
+<script> 
+ import Navbar from './components/Navbar.vue';
+ import Footer from './components/Footer.vue';
+
+export default{
+  components: {
+    Navbar,
+    Footer
+  }
+}
+</script>
+
+<style>
+
+</style>

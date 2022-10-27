bc3f74bdd62818a06662b7ec6e3456b6e2b9a3a1
Marco Antonio Caldeira <caldeiran391@gmail.com>
2022-10-27 12:36:36 -0300

    adicionando

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,20 @@
 <template>
-  <div class="home">
-    <h1>Home</h1>
+  <Banner />
+  <div class="main-container">
+    <h1>Monte o seu burger:</h1>
+    <BurgerForm />
   </div>
 </template>
 
 <script>
+import Banner from "../components/Banner.vue"
+import BurgerForm from "../components/BurgerForm.vue"
 
+export default{
+  name: "Home",
+  components: {
+    Banner,
+    BurgerForm
+  }
+}
 </script>
